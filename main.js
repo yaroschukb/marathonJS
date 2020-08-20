055e7539d6c4a6ee4924144fa4e32de71da68120
Your Name <you@example.com>
2020-08-20 14:29:57 +0300

    Homework-2

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert('Привет Мафонец!!!')
+
+
+
+
+const phone = prompt('Push your number', '')
+
+alert(formattedPhone(phone))
+
+function formattedPhone(phone) {
+	if(phone.length<11){
+		alert ('Push Number!!!')
+	}
+  for (let i=0; i<phone.length; i++)
+  { let result = 
+        phone.charAt(0)+phone.charAt(1)+ ' '+
+        '('+phone.charAt(2)+phone.charAt(3)
+        +phone.charAt(2)+')'+' '+phone.charAt(3)
+        +phone.charAt(4)+phone.charAt(5)+'-'+
+        +phone.charAt(6)+phone.charAt(7)+'-'+
+        +phone.charAt(8)+phone.charAt(9)
+      return result}
+}
+
