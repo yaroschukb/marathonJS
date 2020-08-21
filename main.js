be88836c6ca9818bc171b696ce5ce4727242bfb0
Your Name <you@example.com>
2020-08-21 10:03:12 +0300

    fixed

--- main.js
+++ main.js
@@ -7,17 +7,15 @@ const phone = prompt('Push your number', '')
 alert(formattedPhone(phone))
 
 function formattedPhone(phone) {
-	if(phone.length<11){
-		alert ('Push Number!!!')
-	}
-  for (let i=0; i<phone.length; i++)
-  { let result = 
+  if(phone.length<11){
+    alert ('Push Number!!!')
+  }
+  return result = 
         phone.charAt(0)+phone.charAt(1)+ ' '+
         '('+phone.charAt(2)+phone.charAt(3)
-        +phone.charAt(2)+')'+' '+phone.charAt(3)
-        +phone.charAt(4)+phone.charAt(5)+'-'+
+        +phone.charAt(4)+')'+' '+phone.charAt(5)
         +phone.charAt(6)+phone.charAt(7)+'-'+
-        +phone.charAt(8)+phone.charAt(9)
-      return result}
+        +phone.charAt(8)+phone.charAt(9)+'-'+
+        +phone.charAt(10)+phone.charAt(11)
 }
 
